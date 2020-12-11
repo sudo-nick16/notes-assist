42d2af38f439630fec0b80eb78f08a3c0b1a5f4c
Nikit Singh <singhnikit16@gmail.com>
2020-12-12 03:31:42 +0530

    Update1.0

--- notes-taker.py
+++ notes-taker.py
@@ -6,6 +6,11 @@ from tkinter import *
 import re
 
 root = Tk()
+root.title("Notes Taker")
+root.iconbitmap('./assets/notes.ico')
+img = PhotoImage(file='E:/HPro/assets/tre.png')
+label = Label(root, image=img)
+label.place(x=0, y=0, relwidth=1, relheight=1)
 
 today = str(date.today())
 path = os.path.normpath(os.path.expanduser("~/Desktop"))
@@ -18,10 +23,11 @@ if not isCS:
     os.mkdir("CS")
 
 i = IntVar()
-rb1 = Radiobutton(root, text='Maths', value=1, variable=i)
-rb2 = Radiobutton(root, text='CS', value=2, variable=i)
-rb1.pack()
-rb2.pack()
+rb1 = Radiobutton(root, text='Maths', value=1, variable=i, bg="darkgray")
+rb2 = Radiobutton(root, text='CS', value=2, variable=i, bg="#a0a0a0")
+rb1.pack(pady=(20,10))
+rb2.pack(pady=10)
+
 def makedir():
     if (i.get() == 1):
         os.chdir("Maths")
@@ -38,20 +44,24 @@ def makedir():
         label_1 = Label(root, text="Directory Already Present", width=20, height=4)
         label_1.pack()
 
-button1 = Button(root, text="Make a directory for today", command=makedir)
-button1.pack()
+
 def take_ss():
-    # To capture the screen
+    makedir()
     image = pyscreenshot.grab()
-    # To rename the ss
     now = str(datetime.now())
     file_name = re.sub(r'[^\w]', ' ', now)
-    # To save the screenshot
     image.save(file_name + ".png")
 
 
-button2 = Button(root, text="Take Screenshot", command=take_ss)
-button2.pack()
+button2 = Button(root, text="Take Screenshot", command=take_ss, bg="black", border=0, fg="white", padx=20, pady=6)
+button2.config(activebackground="gray15", activeforeground="white")
+button2.pack(pady=10)
 
+ext = Button(root,text="Exit", command=root.quit, justify="center", activebackground="gray15", activeforeground="white")
+ext.config(padx=20, pady=5, bg="black", fg="white", border=0, relief="raised")
+ext.pack(pady=(10,5))
+
+
+root.resizable(0, 0)
 root.geometry("300x300+120+120")
 root.mainloop()
