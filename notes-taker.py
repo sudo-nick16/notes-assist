81ab1b749b990b6853d8e102fcc7a7d65a082e56
Nikit Singh <singhnikit16@gmail.com>
2020-12-12 14:22:15 +0530

    Update1.1

--- notes-taker.py
+++ notes-taker.py
@@ -22,12 +22,6 @@ if not isM:
 if not isCS:
     os.mkdir("CS")
 
-i = IntVar()
-rb1 = Radiobutton(root, text='Maths', value=1, variable=i, bg="darkgray")
-rb2 = Radiobutton(root, text='CS', value=2, variable=i, bg="#a0a0a0")
-rb1.pack(pady=(20,10))
-rb2.pack(pady=10)
-
 def makedir():
     if (i.get() == 1):
         os.chdir("Maths")
@@ -37,30 +31,40 @@ def makedir():
     if not isT:
         os.mkdir(today)
         os.chdir(today)
-        label_1 = Label(root, text="Directory Created", width=15, height=4)
-        label_1.pack()
+        '''label_1 = Label(root, text="Directory Created", width=15, height=1, bg="#a0a0a0", font="timesnewroman")
+        label_1.pack(pady=(5,0))'''
     else:
         os.chdir(today)
-        label_1 = Label(root, text="Directory Already Present", width=20, height=4)
-        label_1.pack()
+        '''label_1 = Label(root, text="Directory Already Present", width=20, height=1, bg="#a0a0a0")
+        label_1.pack(pady=(5,0))'''
+
+
+i = IntVar()
+rb1 = Radiobutton(root, text='Maths', value=1, variable=i, bg="darkgray", command=makedir)
+rb2 = Radiobutton(root, text='CS', value=2, variable=i, bg="#a0a0a0", command=makedir)
+rb1.pack(pady=(20, 10))
+rb2.pack(pady=10)
 
 
 def take_ss():
-    makedir()
     image = pyscreenshot.grab()
     now = str(datetime.now())
     file_name = re.sub(r'[^\w]', ' ', now)
     image.save(file_name + ".png")
 
 
+'''button1 = Button(root, text="Make Directory", command=makedir, bg="black", border=0, fg="white", padx=20, pady=6)
+button1.config(activebackground="gray15", activeforeground="white")
+button1.pack(pady=(10, 5))'''
+
 button2 = Button(root, text="Take Screenshot", command=take_ss, bg="black", border=0, fg="white", padx=20, pady=6)
 button2.config(activebackground="gray15", activeforeground="white")
-button2.pack(pady=10)
+button2.pack(pady=(10, 5))
 
-ext = Button(root,text="Exit", command=root.quit, justify="center", activebackground="gray15", activeforeground="white")
+ext = Button(root, text="Exit", command=root.quit, justify="center", activebackground="gray15",
+             activeforeground="white")
 ext.config(padx=20, pady=5, bg="black", fg="white", border=0, relief="raised")
-ext.pack(pady=(10,5))
-
+ext.pack(pady=(10, 5))
 
 root.resizable(0, 0)
 root.geometry("300x300+120+120")
